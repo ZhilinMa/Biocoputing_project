1faa955e7411af9b88ed07bda2f8cb1767e0fb67
cturlo <cturlo@nd.edu>
2022-10-11 23:59:28 -0400

    added Project_details.sh to git branch

---
+++ Project_details.sh
@@ -0,0 +1,27 @@
+#Generate a shell script to do the following
+
+#Identify methanogens (McrA+)/proteome
+	#18 mcrA genes
+	#use muscle to generate a HMM of aligned mcrA genes
+	#hmmerbuild to build HMM profile
+	#hmmersearch to use HMM profile to search proteomes
+		#interpret results (E-value, bit score)
+		#E-value - #hits expected by chance, decreases as alignment score increases (want low)
+			#shorter sequences have higher probability of high E-values due to chance
+		#bit score - #required size of database in which current match found by chance
+			-log2 scale - each increase doubles required database size (want this high)
+	#sort by (x)
+	#remove proteomes that fail to match
+	#echo proteome filenames > methanogens.txt
+
+#pH resistance identified: proxy is how many Hsp70 genes present/proteome
+	#22 hsp70 gene files
+	#use muscle to generate a HMM of aligned Hsp70 genes
+	#hmmerbuild to build HMM profile
+	#hmmersearch to use HMM profile to search proteomes
+		#interpret results
+
+#Produce summary table collating results of script
+
+#Provide text file with the names of candidate pH-resistant methanogens
+
