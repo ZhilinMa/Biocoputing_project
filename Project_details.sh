30e0b75079fb3e161b15ec112669867bdc3d67ec
cturlo <cturlo@nd.edu>
2022-10-14 00:06:51 -0400

    Project_details updated clearly with code that produces a summary table of hsp/mcr genes, unsorted

--- Project_details.sh
+++ Project_details.sh
@@ -1,50 +1,61 @@
-#Generate a shell script to do the following
+#Zhilin - these file paths are relative to our respective file structures. Mine is not the same as Joe's
+#Here are the assumptions we are making:
+#You are within the root directory in the CRC
+#You are using exclusively absolute paths for all files
+	#We understand that for orthogonality sake, this is not ideal, but we are still able to get our results this way
+	#If you see "file_path" that is in reference to our respective file path to get to the location of our project files
+		#Individual paths will be written here and commented out, use whatever is in reference to your file structure
 
-#Identify methanogens (McrA+)/proteome
-	#18 mcrA genes
-	#compile genes - return file "mcr.refs"
-for mcr_file in (file_path)
+#Goal 1: Generate reference files for mcrA and hsp70 genes using muscle:
+#18 mcrA genes, 22 hsp70 genes
+
+	for mcr_file in (file_path1)/mcr*.fasta
+	do
+	cat $mcr_file >> mcr.refs
+	done
+
+	for hsp_file in (file_path1)/hsp*.fasta
+	do
+	cat $hsp_file >> hsp.refs
+	done
+
+		#(file_path1)
+			#Chris's: ~/Private/Biocomputing_Project/ref_sequences
+			#Joe's:
+
+#Goal 2: Use MUSCLE to generate a HMM of aligned mcrA and hsp70 genes
+	(file_path2)/muscle -in ~/mcr.refs -out mcr_align
+	(file_path2)/muscle -in ~/hsp.refs -out hsp_align
+		
+		#(file_path2)
+		#Chris's: ~/Private/Biocomputing2022/tools
+		#Joe's:
+
+#Goal 3: Use hmmbuild to build an HMM profile for each gene type
+	(file_path2)/hmmbuild mcrhmm mcr_align
+	(file_path2)/hmmbuild hsphmm hsp_align
+
+#Goal 4: Use hmmsearch in a for loop to identify if genes are present, and if so how frequently; produce summary table
+
+echo "proteome_name", "mcr_count", "hsp_count" > summary_table.csv
+for proteome in (file_path3)/proteome_*.fasta
 do
-cat $mcr_file >> mcr.refs
-	#use muscle to generate a HMM of aligned mcrA genes
-~/Private/Biocomputing/tools/muscle -in ~/mcr.refs -out mcr_align
-
-	#hmmerbuild to build HMM profile
-~/Private/Biocomputing/tools/hmmbuild mcrhmm mcralign
-
-	#hmmersearch to use HMM profile to search proteomes
-		#interpret results (E-value, bit score)
-		#E-value - #hits expected by chance, decreases as alignment score increases (want low)
-			#shorter sequences have higher probability of high E-values due to chance
-		#bit score - #required size of database in which current match found by chance
-			#-log2 scale - each increase doubles required database size (want this high)
-	#sort by (x)
-	#remove proteomes that fail to match
-	#echo proteome filenames > methanogens.txt
-
-#pH resistance identified: proxy is how many Hsp70 genes present
-	#22 hsp70 gene files
-for hsp_file in (path)
-do
-cat $hsp_file >> hsp70.refs
+(file_path2)/hmmsearch --tblout mcr_table mcrhmm $proteome
+(file_path2)/hmmsearch --tblout hsp_table hsphmm $proteome
+proteome_name=$(echo "$proteome")
+mcr_count=$(cat mcr_table|grep -o "WP_*"|wc -l)
+hsp_count=$(cat hsp_table|grep -o "WP_*"|wc -l)
+echo "$proteome_name", "$mcr_count", "$hsp_count">>summary_table.csv
 done
 
-	#use muscle to generate a HMM of aligned Hsp70 genes
-~/Private/Biocomputing2022/tools/muscle - in ~/hsp.refs
-	#hmmerbuild to build HMM profile
-~/Private/Biocomputing/tools/hmmbuild hsphmm hspalign
+		#(file_path3)
+		#Chris's: ~/Private/Biocomputing_Project/proteomes
+		#Joe's:
 
-	#hmmersearch to use HMM profile to search proteomes
-For proteome in ~/Private/Biocomputing2022/Biocomputing_Project/proteomes/proteome_*.fasta
-Do
-~Private/Biocomputing/tools/hmmsearch --tblout hsp_table hsphmm $proteome
-		#interpret results
 
-#Produce summary table collating results of script
+#Goal 5: Sort by presence of mcrA (column 2), presence of hsp70 (column 3)
+	#mcrA >1, produces methane
+	#number of hsp70 increases, increased pH resistance
+#Provide text file with the names of candidate pH-resistant methanogens (i.e. head, to a certain point)
 
-#Provide text file with the names of candidate pH-resistant methanogens
-
-#Joe - I would not run this script right away bc your file system and mine 
-#are likely different; this includes the hmmtools steps and a potential try at the hmmersearch
-#I cannot connect to VPN right now, this is my best try. I will text when I get to my airBNB and if you can't get hmmsearch to work, lmk.
 
